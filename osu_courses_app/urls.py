74270ae746ec22ae0e7174f52cba1f82394fcf58
Pranjal Mittal <mittal.pranjal@gmail.com>
2015-04-25 12:24:41 -0700

    Init project

---
+++ osu_courses_app/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from main import views
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'osu_courses_app.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^/$', views.index, name='index')
+]
