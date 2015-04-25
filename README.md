f2ebf18ce6b4cb54141faee25ebed2e4a5dde4c9
Pranjal Mittal <mittal.pranjal@gmail.com>
2015-04-25 12:41:04 -0700

    Added instructions to start dev server

---
+++ README.md
@@ -0,0 +1,7 @@
+Usage
+-----
+
+    sudo pip install -r requirements.txt
+    python manage.py createsuperuser
+    python manage.py runserver 0.0.0.0:80
+
--- main/models.py
+++ main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    """
+    Contains the JSON question
+    """
+    crn = models.CharField(max_length=16, null=True, blank=True)
+    name = models.CharField(max_length=32, null=True, blank=True)
+    term = models.CharField(max_length=8, null=True, blank=True)
+    professor = models.ForeginKey(Professor)
+
+    def __unicode__(self):
+        return self.name + ': ' + self.term
+
+
+class Professor(models.Model):
+    name = models.CharField(max_length=32, null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+class Major(models.Model):
+    name = models.CharField(max_length=16, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
