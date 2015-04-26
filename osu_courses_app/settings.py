149210227b316e29738745cefb62069e43c3ad3b
Pranjal Mittal <mittal.pranjal@gmail.com>
2015-04-25 21:41:26 -0700

    Changed database to MySQL

--- osu_courses_app/settings.py
+++ osu_courses_app/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'osu_courses_app.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'db',
+        'USER': 'root',
+        'PASSWORD': os.environ['MYSQL_PASSWORD'],
+        'HOST': '54.213.102.221',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
